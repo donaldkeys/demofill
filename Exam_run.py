3d545e182d2c3c0e251ced20c2109859e049c969
user pc <donaldkeys2k13@yahoo.co.uk>
2022-04-16 20:38:47 +0100

    this my second try

--- Exam_run.py
+++ Exam_run.py
@@ -1,19 +1,19 @@
 sales = int(input("Sales made: "))
 
-shortage = (10 - sales)
+shortage = (11 - sales)
 weekly_payment = 1000
 
-
-if sales >= 10:
+# For_those_that met the target.
+if sales >= 11:
 
     print(f"£{1000 + 250}")
     print("You did well, Great job!")
 
+# For_those_that didn't meet the target.
 elif sales < 10:
 
-    print(f"You have {shortage} shortage(s)")
-    print
+    print(f"You have {shortage} shortage(s)\n")
 
+# For_those_that had a regular task with no extra or less.
 else:
-    print(f"{weekly_payment} Pounds")
-
+    print(f" £{weekly_payment} \n You can do better, if you put in more effort!")
