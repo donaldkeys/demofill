b28501d308f5657d2bb1f6404c88cc9febd6564e
user pc <donaldkeys2k13@yahoo.co.uk>
2022-04-16 20:47:11 +0100

    this my second try

--- Exam.py
+++ Exam.py
@@ -1,7 +1,9 @@
 salespeople_payment = 1000
 sales_exceeded_10 = 250 + salespeople_payment
-sales_exceed_10=False
+
+#sales_exceed_10=False
 sales_exceed_10=True
+
 if sales_exceed_10:
     print(f"£{sales_exceeded_10}")
     print("Good job!")
@@ -9,3 +11,4 @@ elif sales_exceed_10 != 250:
     print("you didn't meet up!")
 else:
     print("salespeople_payment")
+
