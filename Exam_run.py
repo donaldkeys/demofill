ce4ee83c11681f38bf64827cb44643f525988cb5
user pc <donaldkeys2k13@yahoo.co.uk>
2022-04-16 20:47:11 +0100

    this my second try

--- Exam_run.py
+++ Exam_run.py
@@ -6,13 +6,12 @@ weekly_payment = 1000
 # For_those_that met the target.
 if sales >= 11:
 
-    print(f"£{1000 + 250}")
-    print("You did well, Great job!")
+    print(f"£{1000 + 250}\n You did well, Great job!")
 
 # For_those_that didn't meet the target.
 elif sales < 10:
 
-    print(f"You have {shortage} shortage(s)\n")
+    print(f"You have {shortage} shortage(s)")
 
 # For_those_that had a regular task with no extra or less.
 else:
