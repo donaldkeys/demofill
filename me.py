c0b37948d891283eae8d64fc80042c5859d46384
user pc <donaldkeys2k13@yahoo.co.uk>
2022-04-16 12:50:05 +0100

    this my second try

--- me.py
+++ me.py
@@ -1,1 +1,8 @@
-print("hello git")
+salespeople_payment = 1000
+sales_exceed_10 = 250 + salespeople_payment
+
+if sales_exceed_10:
+    print(f"{sales_exceed_10}")
+    print("Good job!")
+else:
+    print("Sales shortage")
