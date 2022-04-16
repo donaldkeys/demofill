b9eeb89cf578e930b47e7d6ba9a17d3a3e75bb34
user pc <donaldkeys2k13@yahoo.co.uk>
2022-04-16 13:02:37 +0100

    this my second try

---
+++ Exam.py
@@ -0,0 +1,8 @@
+salespeople_payment = 1000
+sales_exceed_10 = 250 + salespeople_payment
+
+if sales_exceed_10:
+    print(f"£{sales_exceed_10}")
+    print("Good job!")
+else:
+    print("Sales shortage")
