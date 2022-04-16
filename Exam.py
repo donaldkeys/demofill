f114a00ba684816d5eab2131dcbbe44ed3cc4ca0
user pc <donaldkeys2k13@yahoo.co.uk>
2022-04-16 15:06:24 +0100

    this my second try

--- Exam.py
+++ Exam.py
@@ -1,8 +1,11 @@
 salespeople_payment = 1000
-sales_exceed_10 = 250 + salespeople_payment
-
+sales_exceeded_10 = 250 + salespeople_payment
+sales_exceed_10=False
+sales_exceed_10=True
 if sales_exceed_10:
-    print(f"£{sales_exceed_10}")
+    print(f"£{sales_exceeded_10}")
     print("Good job!")
+elif sales_exceed_10 != 250:
+    print("you didn't meet up!")
 else:
-    print("Sales shortage")
+    print("salespeople_payment")
--- me.py
+++ me.py
@@ -3,6 +3,6 @@ sales_exceed_10 = 250 + salespeople_payment
 
 if sales_exceed_10:
     print(f"{sales_exceed_10}")
-    print("Good job!")
+    print("Good job ")
 else:
     print("Sales shortage")
